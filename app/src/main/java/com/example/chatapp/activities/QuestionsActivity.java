e3a77c8651bc3a7b31c4266d173969e4bb52e88c
nameerja23-meet <nemojabara@gmail.com>
2023-07-17 08:34:18 +0300

    Revert "Revert "chat completed began questions activitiy""
    This reverts commit 7def5baf687084a23a56fea98c3dfe122043b65e.

---
+++ app/src/main/java/com/example/chatapp/activities/QuestionsActivity.java
@@ -0,0 +1,24 @@
+package com.example.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityQuestionsBinding;
+import com.example.chatapp.utilities.PreferenceManager;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    private ActivityQuestionsBinding binding;
+    private PreferenceManager preferenceManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityQuestionsBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setContentView(binding.getRoot());
+    }
+
+
+}
